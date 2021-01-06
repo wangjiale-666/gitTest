c9d189c7575f807a6b4d3c47c81cc10ec416d52d
jerry <jerry@qq.com>
2021-01-06 15:20:36 +0800

    13

--- src/main/java/com/gitTest/gitTest/Ctr1.java
+++ src/main/java/com/gitTest/gitTest/Ctr1.java
@@ -6,13 +6,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Ctr1 {
-	
 
 	public String name1="张三";
 	
 	public String name3="王五";
-	
-	
+
 	@RequestMapping("/aa")
 	public String aa(){
 		return "呵呵";
