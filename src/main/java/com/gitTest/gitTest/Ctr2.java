3d21f1681844ed61641023cbe3d560d8cb1a5b1c
jerry <jerry@qq.com>
2021-01-06 15:13:02 +0800

    s

--- src/main/java/com/gitTest/gitTest/Ctr2.java
+++ src/main/java/com/gitTest/gitTest/Ctr2.java
@@ -1,16 +1,9 @@
 package com.gitTest.gitTest;
 
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Ctr2 {
-	
-	public String name1="张三";
-	
-	public String name3="王五";
-	
-	@RequestMapping("/bb")
 	public String bb(){
 		return "bb";
 	}
